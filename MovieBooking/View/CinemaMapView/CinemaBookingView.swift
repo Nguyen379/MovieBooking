dd95b73d7587cf070493b760672fb3dfa3a387d7
Nguyen <anhnguyenle379@gmail.com>
2024-05-19 23:43:28 +1000

    Update CinemaBookingView.swift

--- MovieBooking/View/CinemaMapView/CinemaBookingView.swift
+++ MovieBooking/View/CinemaMapView/CinemaBookingView.swift
@@ -10,12 +10,17 @@ import SwiftUI
 //Booking options for selected cinema
 struct CinemaBookingView: View {
     var cinema: Cinema
-    
+
     var body: some View {
         VStack(spacing: 20) {
-            Text("Would you like to book at \(cinema.name)?")
-            NavigationLink("Yes, book now!", destination: Text("Booking Screen (To be implemented by another team member)"))
-            .padding()
+            Text("Would you like to book at (cinema.name)?")
+            NavigationLink(destination: BookingView()) {
+                Text("Yes, book now!")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
         }
         .navigationBarTitle(Text(cinema.name), displayMode: .inline) // Adds a title to the booking view
         .padding()
