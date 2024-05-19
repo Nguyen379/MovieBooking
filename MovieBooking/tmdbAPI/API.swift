ed8ee9b21e70e9a8990df2aa84138c3648d754bc
Nguyen <anhnguyenle379@gmail.com>
2024-05-19 17:43:30 +1000

    API update

---
+++ MovieBooking/tmdbAPI/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  MovieBooking
+//
+//  Created by Le Anh Nguyen on 5/18/24.
+//
+
+import Foundation
+
+struct API {
+    static let key = "3ee7b04be548b190992a01d0ca1fe465"
+}
